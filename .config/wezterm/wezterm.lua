9ce9c6297cafb6ab9b05ba12c16425be8b60f00d
Daniel Wijnands <daniel@Daniels-MacBook-Pro.local>
2025-12-02 19:06:59 +0100

    add wezterm and aerospace

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+local wezterm = require 'wezterm'
+local commands = require 'commands'
+
+local config = wezterm.config_builder()
+
+-- Font settings
+config.font_size = 14
+config.line_height = 1.2
+
+-- Colors
+config.color_scheme = 'Catppuccin Mocha'
+config.enable_scroll_bar = true
+-- Appearance
+config.cursor_blink_rate = 0
+-- config.window_decorations = 'RESIZE'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.macos_window_background_blur = 40
+
+-- Miscellaneous settings
+config.max_fps = 120
+config.prefer_egl = true
+
+-- Custom commands
+wezterm.on('augment-command-palette', function()
+  return commands
+end)
+
+return config
